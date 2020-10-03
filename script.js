9a508877187f30858de5de5666e71cd0c3243ea1
Blackjack2020 <69491789+devonp702@users.noreply.github.com>
2020-10-02 23:28:05 -0600

    sets local storage, and changed to dayjs

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
-console.log(moment().calendar());
+$("document").ready(function () {
+  var month = dayjs().month() + 1; // Time stuff
+  var day = dayjs().date();
+  var year = dayjs().year();
+  var hour = dayjs().hour();
+  var storage = {
+    9: "",
+    10: "",
+    11: "",
+    12: "",
+    13: "",
+    14: "",
+    15: "",
+    16: "",
+    17: ""
+  };
+  var load = window.localStorage.getItem("save");
+  storage = JSON.parse(load);
+  console.log(storage);
+  $("#currentDay").text("Current Day is: " + month + "/" + day + "/" + year);
+  for (var i = 0; i < storage.length; i++) {
+    console.log(storage[i]);
+  }
+  $(".hour").each(function (index) {
+    $(this).attr("data-time", 9 + index); // set attribute to time in military time
+  });
+  $(".hour").each(function () {
+    var temp = $(this).attr("data-time");
+    //console.log("Page: " + dayjs().hour(temp));
+    //console.log("Now: " + hour);
+  });
+  //Data Handling
+  $("button").on("click", function () {
+    var str = $(this).prev().val();
+    var idx = $(this).prev().prev().attr("data-time");
+    storage[idx] = str;
+    var cookie = JSON.stringify(storage);
+    window.localStorage.setItem("save", cookie)
+  });
+});
